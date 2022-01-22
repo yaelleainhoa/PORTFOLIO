e1a69de56bb89ee352491ba24c3f5e764287975f
Yaelle <yaellesaraleguitran@gmail.com>
2022-01-22 01:14:19 +0100

    [HOMEPAGE] transition presentation/description

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
 window.addEventListener("wheel", function() {
     let pageY = window.scrollY;
-    document.querySelector(".nom").style.transform = `translateY(${pageY * 0.2}px)`;
-    //document.querySelector(".bulle").style.transform = `translateY(${pageY * 0.1}px)`;
-    document.querySelector(".statut").style.transform = `translateY(${pageY*0.3}px)`;
+    document.querySelector(".nom").style.transform = `translateY(${pageY * 0.15}px)`;
+    document.querySelector(".statut").style.transform = `translateY(${pageY*0.2}px)`;
+    // document.querySelector(".infos").style.transform = `rotate(-90deg) translateY(${pageY*0.01}px)`;
+    document.querySelector(".nom").style.transform = `translateX(${-100})px)`;
+    document.querySelector(".nom").style.couleur = 'pink';
 })
+
+let projets = ['babyrun', 'shaderlab', 'portfolio', 'blender', 'coquelicot'];
+
+function unfocus(projet){
+    for(proj in projets){
+        let projetEnCours = document.querySelector('.'+projets[proj]);
+        if(projet!=projetEnCours && projetEnCours.classList.contains("focus")){
+            projetEnCours.classList.remove("focus");
+        }
+    }
+}
+
+for(proj in projets){
+    let presentation = document.querySelector('.'+projets[proj]+' .presentation');
+    let description = document.querySelector('.'+projets[proj]+' .description');
+
+    let projet = document.querySelector('.'+projets[proj]);
+    presentation.addEventListener('click', () => {
+                                            projet.classList.toggle("focus");
+                                            unfocus(projet);
+                                        });
+
+    description.addEventListener('click', () => {
+        projet.classList.toggle("focus");
+        unfocus(projet);
+    });
+}
+
