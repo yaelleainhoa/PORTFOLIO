f7c4a3cdd17ff944c025f58322cb4c1884f51598
Yaelle <yaellesaraleguitran@gmail.com>
2022-01-30 18:20:46 +0100

    [HOMEPAGE] scroll indication + refresh button

--- script.js
+++ script.js
@@ -7,6 +7,37 @@
 //     document.querySelector(".nom").style.couleur = 'pink';
 // })
 
+function refreshPage(){
+    window.location.reload();
+} 
+
+window.addEventListener("wheel", function() {
+    let pageY = window.scrollY;
+    document.querySelector(".transition").style.transform = `translateY(${pageY}px)`;
+    document.querySelector(".transition").classList.add("appear");
+})
+
+// Setup isScrolling variable
+var isScrolling;
+
+// Listen for scroll events
+window.addEventListener('scroll', function ( event ) {
+
+	// Clear our timeout throughout the scroll
+	window.clearTimeout( isScrolling );
+
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+
+		// Run the callback
+        document.querySelector(".transition").classList.remove("appear");
+
+	}, 500);
+
+}, false);
+
+
+
 let projets = ['babyrun', 'shaderlab', 'portfolio', 'blender', 'coquelicot'];
 
 function unfocus(projet){
