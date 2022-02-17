6bb7229f6d8af352aa7f8a4f09c212cb8b79f68b
Yaelle <yaellesaraleguitran@gmail.com>
2022-02-17 23:26:46 +0100

    [PHONE] responsive first try

--- script.js
+++ script.js
@@ -11,33 +11,6 @@ function refreshPage(){
     window.location.reload();
 } 
 
-// window.addEventListener("wheel", function() {
-//     let pageY = window.scrollY;
-//     document.querySelector(".transition").style.transform = `translateY(${pageY}px)`;
-//     document.querySelector(".transition").classList.add("appear");
-// })
-
-// Setup isScrolling variable
-// var isScrolling;
-
-// // Listen for scroll events
-// window.addEventListener('scroll', function ( event ) {
-
-// 	// Clear our timeout throughout the scroll
-// 	window.clearTimeout( isScrolling );
-
-// 	// Set a timeout to run after scrolling ends
-// 	isScrolling = setTimeout(function() {
-
-// 		// Run the callback
-//         document.querySelector(".transition").classList.remove("appear");
-
-// 	}, 500);
-
-// }, false);
-
-
-
 let projets = ['babyrun', 'shaderlab', 'portfolio', 'blender', 'coquelicot'];
 
 var isUnfocused;
@@ -79,12 +52,6 @@ for(proj in projets){
     });
 }
 
-// var swiper = new Swiper(".mySwiper", {
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
 
 var swiper = new Swiper(".mySwiper", {
     direction: "vertical",
